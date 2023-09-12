34201b7db2f9733186459ddb538712c135470b66
Truong Dang Nam <144240095+DangNam030204@users.noreply.github.com>
2023-09-12 11:36:45 +0700

    Add files via upload

---
+++ HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12522071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Họ tên, mã sinh viên, lớp, username tài khoản github đã mở, địa chỉ email
+            Console.WriteLine("Trương Đăng Nam\t12522071\t12422TN\tDangNam030204\tdangnam030204@gmail.com");
+            Console.ReadKey();
+        }
+    }
+}
